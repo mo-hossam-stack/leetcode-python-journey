b01b8add8cd2587351002097c2732440af5fe0f9
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-07-18 23:42:22 +0300

    feat(arrays): add solution for '136_Single_Number'

---
+++ arrays/136_Single_Number.py
@@ -0,0 +1,10 @@
+# Problem: Single Number
+# LeetCode: https://leetcode.com/problems/single-number/
+# Difficulty: Easy
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0 
+        for num in nums:
+            res ^= num
+        return res
