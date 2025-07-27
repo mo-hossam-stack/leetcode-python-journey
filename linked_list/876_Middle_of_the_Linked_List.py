c8dbadaca55a5630bc13feef0c4f9b9fe26b25be
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-07-27 21:10:02 +0300

    feat(fundamentals): add solution for '2239_Find_Closest_Number_to_Zero.py'

---
+++ linked_list/876_Middle_of_the_Linked_List.py
@@ -0,0 +1,17 @@
+# Problem: Middle of the Linked List
+# LeetCode: https://leetcode.com/problems/middle-of-the-linked-list/
+# Difficulty: Easy
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
