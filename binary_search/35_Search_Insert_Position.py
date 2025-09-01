b8212e7232581b488d1a06909d737b7e169d7df6
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-09-01 17:41:09 +0300

    feat(binary_search): add solution for '35_Search_Insert_Position.py'

---
+++ binary_search/35_Search_Insert_Position.py
@@ -0,0 +1,16 @@
+# Problem: Search Insert Position
+# LeetCode: https://leetcode.com/problems/search-insert-position/
+# Difficulty: Easy
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
