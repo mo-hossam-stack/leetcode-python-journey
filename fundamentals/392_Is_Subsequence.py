e0d403c8814ebddd1fa828bb858462ecf98505f4
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-09-08 18:47:34 +0300

    feat(fundamentals): add solution for '392_Is_Subsequence.py'

---
+++ fundamentals/392_Is_Subsequence.py
@@ -0,0 +1,18 @@
+# Problem: Is Subsequence
+# LeetCode: https://leetcode.com/problems/is-subsequence/
+# Difficulty: Easy
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        S, T = len(s), len(t)
+        if s == '':
+            return True
+        if S > T:
+            return False
+        j = 0
+        for i in range(T):
+            if t[i] == s[j]:
+                if j == S - 1:
+                    return True
+                j += 1
+        return False
