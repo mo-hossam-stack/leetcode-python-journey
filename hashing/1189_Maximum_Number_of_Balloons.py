ff946552efb59be63bb27f4bdab31a5d2a62902d
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-09-11 18:38:24 +0300

    feat(hashing): add solution for '1189_Maximum_Number_of_Balloons.py'

---
+++ hashing/1189_Maximum_Number_of_Balloons.py
@@ -0,0 +1,18 @@
+# Problem: Maximum Number of Balloons
+# LeetCode: https://leetcode.com/problems/maximum-number-of-balloons/
+# Difficulty: Easy
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        counter = defaultdict(int)
+        balloon = 'balloon'
+        for c in text:
+            if c in balloon:
+                counter[c] += 1
+
+        if any(c not in counter for c in balloon):
+            return 0
+        else:
+            return min(counter['b'],counter['a'],
+                counter['l'] //2,counter['o'] //2,counter['n']
+            )
