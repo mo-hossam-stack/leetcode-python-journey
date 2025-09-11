b5b9c2d53cecf369163d6fa77d4af920a593b348
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-09-11 20:24:52 +0300

    feat(arrays): add solution for '58_Length_of_Last_Word.py'

---
+++ arrays/58_Length_of_Last_Word.py
@@ -0,0 +1,7 @@
+# Problem: Length of Last Word
+# LeetCode: https://leetcode.com/problems/length-of-last-word/
+# Difficulty: Easy
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.strip().split(' ')[-1])
