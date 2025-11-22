8dce295236feeda5fa13990bb3e6df297d47977e
mo-hossam-stack <hamo08521hamo@gmail.com>
2025-11-22 13:30:06 +0200

    feat(arrays): add solution for '3190_Minimum_Operations_to_Make_the_Array_Good.py'

---
+++ arrays/3190_Minimum_Operations_to_Make_the_Array_Good.py
@@ -0,0 +1,7 @@
+# Problem: Minimum Operations to Make the Array Good
+# LeetCode: https://leetcode.com/problems/minimum-operations-to-make-the-array-good/
+# Difficulty: Easy
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        return sum(min(num % 3, 3 - num % 3) for num in nums)
