ccdd63e14b21663a1ed0f30bfb50ce72bd65a98d
mo-hossam-stack <hamo08521hamo@gmail.com>
2025-12-01 03:47:18 +0200

    feat(arrays): add solution for '3759_Count_Elements_With_at_Least_K_Greater_Values.py'

---
+++ arrays/3759_Count_Elements_With_at_Least_K_Greater_Values.py
@@ -0,0 +1,17 @@
+Problem: Count Elements With at Least K Greater Values
+LeetCode: https://leetcode.com/problems/count-elements-with-at-least-k-greater-values/description/
+
+Difficulty: Medium
+
+class Solution:
+    def countElements(self, nums: List[int], k: int) -> int:
+        if k==0:return len(nums)
+        nums.sort()
+
+        n = len(nums)
+        threshold = nums[n - k]
+        
+        res = 0
+        for num in nums:
+            if num < threshold:res+=1
+        return res
