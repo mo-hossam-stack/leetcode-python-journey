4f825080b45f6cff57450107d24e3a7959fb3b17
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-01-24 15:25:18 +0200

    feat(fundamentals): add solution for '22_Generate_Parentheses.py'

---
+++ fundamentals/22_Generate_Parentheses.py
@@ -0,0 +1,20 @@
+# Problem: Generate Parentheses
+# LeetCode: https://leetcode.com/problems/generate-parentheses/
+# Difficulty: Medium
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def dfs(left, right, s):
+            if len(s) == n * 2:
+                res.append(s)
+                return 
+
+            if left < n:
+                dfs(left + 1, right, s + '(')
+
+            if right < left:
+                dfs(left, right + 1, s + ')')
+
+        res = []
+        dfs(0, 0, '')
+        return res
