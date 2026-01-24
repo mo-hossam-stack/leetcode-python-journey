7a4a5af0aec8cf74b83720e706aff4e6fd9ce5f0
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-01-24 15:33:33 +0200

    feat(arrays): add solution for '27_Remove_Element.py'

---
+++ arrays/27_Remove_Element.py
@@ -0,0 +1,12 @@
+# Problem: Remove Element
+# LeetCode: https://leetcode.com/problems/remove-element/
+# Difficulty: Easy
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
