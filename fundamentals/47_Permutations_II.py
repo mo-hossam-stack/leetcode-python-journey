89821fa74c23962b8b10e634708206033ad9a2db
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-02-03 14:49:43 +0200

    feat(fundamentals): add solution for '47_Permutations_II.py'

---
+++ fundamentals/47_Permutations_II.py
@@ -0,0 +1,24 @@
+# Problem: 47. Permutations II
+# LeetCode: https://leetcode.com/problems/permutations-ii/
+# Difficulty: Medium
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        results = []
+
+        def backtrack(comb, counter):
+            if len(comb) == len(nums):
+                results.append(list(comb))
+                return
+
+            for num in counter:
+                if counter[num] > 0:
+                    comb.append(num)
+                    counter[num] -= 1
+                    backtrack(comb, counter)
+                    comb.pop()
+                    counter[num] += 1
+
+        backtrack([], Counter(nums))
+
+        return results
