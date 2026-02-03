3e8603202b90d1f059b5758572d729cc0fd614c6
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-02-03 14:35:13 +0200

    feat(dp): add solution for '45_Jump_Game_II.py'

---
+++ dp/45_Jump_Game_II.py
@@ -0,0 +1,16 @@
+# Problem: 45. Jump Game II
+# LeetCode: https://leetcode.com/problems/jump-game-ii/
+# Difficulty: Medium
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        current = 0
+        farthest = 0
+        jumps = 0
+        for i in range(n-1):
+            farthest = max(farthest, nums[i] + i)
+            if i == current:
+                jumps += 1
+                current = farthest
+        return jumps
