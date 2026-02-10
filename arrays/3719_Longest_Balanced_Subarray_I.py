e01d2d21b65bfe081d7e7e27cd6d20c0822482af
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-02-10 12:36:45 +0200

    feat(arrays): add solution for '3719_Longest_Balanced_Subarray_I.py'

---
+++ arrays/3719_Longest_Balanced_Subarray_I.py
@@ -0,0 +1,29 @@
+# Problem: 3719. Longest Balanced Subarray I
+# LeetCode: https://leetcode.com/problems/longest-balanced-subarray-i/
+# Difficulty: Medium
+
+
+
+
+
+class Solution:
+    def longestBalanced(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = 0
+
+        seen = [0] * (max(nums) + 1)
+
+        for i in range(n):
+            if n - i <= res: break
+            A = [0, 0]
+            
+            for j in range(i, n):
+                val = nums[j]
+                if seen[val] != i + 1:
+                    seen[val] = i + 1
+                    A[val & 1] += 1
+
+                if A[0] == A[1]:
+                    res = max(res, j - i + 1)
+
+        return res
