884b0dcfd033545ceae7b4319803ba27ffc9f8b4
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-02-24 20:36:38 +0200

    feat(arrays): add solution for '77_Combinations.py'

---
+++ arrays/77_Combinations.py
@@ -0,0 +1,21 @@
+# Problem: 77. Combinations
+# LeetCode: https://leetcode.com/problems/combinations/
+# Difficulty: Medium
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        comb = []
+
+        def backtrack(start):
+            if len(comb) == k:
+                res.append(comb[:])
+                return
+            
+            for num in range(start, n + 1):
+                comb.append(num)
+                backtrack(num + 1)
+                comb.pop()
+
+        backtrack(1)
+        return res
