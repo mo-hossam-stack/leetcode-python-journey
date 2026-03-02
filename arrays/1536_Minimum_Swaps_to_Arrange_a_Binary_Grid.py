9362014405fb304e39793337ed9fa4e1af83a24c
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-03-02 20:25:34 +0200

    feat(arrays): add solution for '1536_Minimum_Swaps_to_Arrange_a_Binary_Grid.py'

---
+++ arrays/1536_Minimum_Swaps_to_Arrange_a_Binary_Grid.py
@@ -0,0 +1,30 @@
+# Problem: 1536. Minimum Swaps to Arrange a Binary Grid
+# LeetCode: https://leetcode.com/problems/minimum-swaps-to-arrange-a-binary-grid/
+# Difficulty: Medium
+
+class Solution:
+    def minSwaps(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        pos = [-1] * n
+        for i in range(n):
+            for j in range(n - 1, -1, -1):
+                if grid[i][j] == 1:
+                    pos[i] = j
+                    break
+
+        ans = 0
+        for i in range(n):
+            k = -1
+            for j in range(i, n):
+                if pos[j] <= i:
+                    ans += j - i
+                    k = j
+                    break
+
+            if k != -1:
+                for j in range(k, i, -1):
+                    pos[j], pos[j - 1] = pos[j - 1], pos[j]
+            else:
+                return -1
+
+        return ans
