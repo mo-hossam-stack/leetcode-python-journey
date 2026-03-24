6c7f1b57e925cf1c87676992df877fe7ada5456c
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-03-24 23:03:19 +0200

    feat(fundamentals): add solution for '90_Subsets_II.py'

---
+++ fundamentals/90_Subsets_II.py
@@ -0,0 +1,25 @@
+# Problem: 90. Subsets II
+# LeetCode: https://leetcode.com/problems/subsets-ii/
+# Difficulty: Medium
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+        nums.sort()
+        
+        def create_subset(i):
+            if i == len(nums):
+                res.append(subset[:])
+                return
+            
+            subset.append(nums[i])
+            create_subset(i+1)
+            subset.pop()
+            while i + 1 < len(nums) and nums[i] == nums[i+1]:
+                i += 1
+                
+            create_subset(i+1)
+        
+        create_subset(0)
+        return res
