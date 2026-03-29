db2f9837aff492d4eaf45716957f73791bfa6897
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-03-29 18:56:38 +0200

    feat(fundamentals): add solution for '2839_Check_if_Strings_Can_be_Made_Equal_With_Operations_I.py'

---
+++ fundamentals/2839_Check_if_Strings_Can_be_Made_Equal_With_Operations_I.py
@@ -0,0 +1,10 @@
+# Problem: 2839. Check if Strings Can be Made Equal With Operations I
+# LeetCode: https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/
+# Difficulty: Easy
+
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        return ((s1[0] == s2[0] and s1[2] == s2[2]) or
+                (s1[0] == s2[2] and s1[2] == s2[0])) and \
+               ((s1[1] == s2[1] and s1[3] == s2[3]) or
+                (s1[1] == s2[3] and s1[3] == s2[1]))
