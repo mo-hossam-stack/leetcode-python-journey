ba7fab0d5d8c1112316f0ca368a049cef2004685
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-04-22 16:34:38 +0200

    feat(fundamentals): add solution for '2452_Words_Within_Two_Edits_of_Dictionary.py'

---
+++ fundamentals/2452_Words_Within_Two_Edits_of_Dictionary.py
@@ -0,0 +1,23 @@
+# Problem: 2452. Words Within Two Edits of Dictionary
+# LeetCode: https://leetcode.com/problems/words-within-two-edits-of-dictionary/
+# Difficulty: Medium
+
+class Solution:
+    def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
+        def _get_distance(s1, s2):
+            cnt = 0
+            for i in range(len(s1)):
+                if s1[i] != s2[i]:
+                    cnt += 1
+                if cnt == 3:
+                    return False
+            return True
+
+        good = []
+        for query in queries:
+            for d in dictionary:
+                dist = _get_distance(query, d)
+                if dist:
+                    good.append(query)
+                    break
+        return good
