5ca05a04d396d59b9b4bb231139b0efa4a1a62c8
mo-hossam-stack <hamo08521hamo@gmail.com>
2026-05-03 22:28:33 +0300

    feat(dp): add solution for '123_Best_Time_to_Buy_and_sell_Stock_III.py'

---
+++ dp/123_Best_Time_to_Buy_and_sell_Stock_III.py
@@ -0,0 +1,19 @@
+# Problem: 123. Best Time to Buy and Sell Stock III
+# LeetCode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+# Difficulty: Hard
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:        
+        buy1 = -prices[0]
+        sell1 = 0
+        buy2 = -prices[0]
+        sell2 = 0
+
+        for i in range(1, len(prices)):
+            price = prices[i]
+
+            buy1 = max(buy1, -price)
+            sell1 = max(sell1, buy1 + price)
+            buy2 = max(buy2, sell1 - price)
+            sell2 = max(sell2, buy2 + price)
+        
+        return sell2
