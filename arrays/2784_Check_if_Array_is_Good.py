8f0d1eee75a64c09f756e725bf8e036238efb115
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-05-14 22:01:13 +0300

    feat(arrays): add solution for '2784_Check_if_Array_is_Good.py'

---
+++ arrays/2784_Check_if_Array_is_Good.py
@@ -0,0 +1,11 @@
+# Problem: 2784. Check if Array is Good
+# LeetCode: https://leetcode.com/problems/check-if-array-is-good/
+# Difficulty: Easy
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        nums.sort()
+        n = len(nums) - 1
+        for i in range(n):
+            if nums[i] != i + 1:
+                return False
+        return nums[n] == n
