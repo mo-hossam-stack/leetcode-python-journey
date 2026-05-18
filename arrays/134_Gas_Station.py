b3baf8ff4d0568b78ad7e5239fe5ed70b4ad5989
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-05-18 19:44:00 +0300

    feat(arrays): add solution for '134_Gas_Station.py'

---
+++ arrays/134_Gas_Station.py
@@ -0,0 +1,17 @@
+<# Problem: 134. Gas Station
+# LeetCode: https://leetcode.com/problems/gas-station/
+# Difficulty: Medium
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+                
+        curernt_gas = 0
+        start = 0
+        for i in range(len(gas)):
+            curernt_gas += gas[i] - cost[i]
+            if curernt_gas < 0:
+                curernt_gas = 0
+                start = i + 1
+
+        return start
