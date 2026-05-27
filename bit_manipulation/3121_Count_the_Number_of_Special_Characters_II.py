7c12a7547d9eb8010aa2ee76e1442fae647e606a
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-05-27 23:09:00 +0300

    feat(bit_manipulation): add solution for '3121_Count_the_Number_of_Special_Characters_II.py'

---
+++ bit_manipulation/3121_Count_the_Number_of_Special_Characters_II.py
@@ -0,0 +1,13 @@
+# Problem: 3121. Count the Number of Special Characters II
+# LeetCode: https://leetcode.com/problems/count-the-number-of-special-characters-ii/
+# Difficulty: Medium
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        A = [[False, False] for _ in range(27)]
+
+        for ch in word:
+            i = ord(ch) & 31
+            c = ord(ch) >> 5 & 1
+            A[i][c] = not (c and A[i][0])
+
+        return sum(u and v for u, v in A)
