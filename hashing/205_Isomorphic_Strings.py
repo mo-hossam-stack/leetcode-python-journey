8153fc2e61dc6f1e33b38c418ba9314bb3d4cd86
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-05-31 16:14:56 +0300

    feat(hashing): add solution for '205_Isomorphic_Strings.py'

---
+++ hashing/205_Isomorphic_Strings.py
@@ -0,0 +1,19 @@
+# Problem: 205. Isomorphic Strings
+# LeetCode: https://leetcode.com/problems/isomorphic-strings/
+# Difficulty: Easy
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        char_index_s = {}
+        char_index_t = {}
+
+        for i in range(len(s)):
+            if s[i] not in char_index_s:
+                char_index_s[s[i]] = i
+
+            if t[i] not in char_index_t:
+                char_index_t[t[i]] = i
+            
+            if char_index_s[s[i]] != char_index_t[t[i]]:
+                return False
+
+        return True
