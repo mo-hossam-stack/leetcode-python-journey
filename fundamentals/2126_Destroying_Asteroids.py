1ba85024a3fc07a270a922fcc742661d723e0937
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-05-31 16:04:20 +0300

    feat(fundamentals): add solution for '2126_Destroying_Asteroids.py'

---
+++ fundamentals/2126_Destroying_Asteroids.py
@@ -0,0 +1,17 @@
+# Problem: 2126. Destroying Asteroids
+# LeetCode: https://leetcode.com/problems/destroying-asteroids/
+# Difficulty: Medium
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        xmax=max(asteroids)
+        freq=[0]*(1+xmax)
+        for x in asteroids:
+            freq[x]+=1
+        planet=mass
+        for x, f in enumerate(freq):
+            if f==0:
+                continue
+            if x>planet:
+                return False
+            planet+=x*f
+        return True
