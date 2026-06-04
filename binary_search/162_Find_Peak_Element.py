3332ed181fb754678b0879a9f42e809e031d9f42
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-06-04 16:50:58 +0300

    feat(binary_search): add solution for '162_Find_Peak_Element.py'

---
+++ binary_search/162_Find_Peak_Element.py
@@ -0,0 +1,24 @@
+# Problem: 162. Find Peak Element
+# LeetCode: https://leetcode.com/problems/find-peak-element/
+# Difficulty: Medium
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        left = 0
+        right = n - 1
+        if len(nums) < 3:
+            return nums.index(max(nums))
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if mid > 0 and mid < n - 1 and nums[mid - 1] < nums[mid] > nums[mid + 1]:
+                return mid
+
+            if nums[mid] < nums[mid + 1]:
+                left = mid + 1
+
+            else:
+                right = mid
+
+        return left
