838a34fd78059131dfb9e5b1e8601ebedc9e5d89
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-06-06 20:33:03 +0300

    feat(fundamentals): add solution for '2574_Left_and_Right_Sum_Differences.py'

---
+++ fundamentals/2574_Left_and_Right_Sum_Differences.py
@@ -0,0 +1,19 @@
+# Problem: 2574. Left and Right Sum Differences
+# LeetCode: https://leetcode.com/problems/left-and-right-sum-differences/
+# Difficulty: Easy
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [0] * n
+
+        left_sum = 0
+        for i in range(n):
+            ans[i] = left_sum
+            left_sum += nums[i]
+
+        right_sum = 0
+        for i in range(n - 1, -1, -1):
+            ans[i] = abs(ans[i] - right_sum)
+            right_sum += nums[i]
+
+        return ans
