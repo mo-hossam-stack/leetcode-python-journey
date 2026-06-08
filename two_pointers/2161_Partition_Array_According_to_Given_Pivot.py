df1818c576c0914b1b97fab310a6c4055c853af5
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-06-08 22:09:31 +0300

    feat(two_pointers): add solution for '2161_Partition_Array_According_to_Given_Pivot.py'

---
+++ two_pointers/2161_Partition_Array_According_to_Given_Pivot.py
@@ -0,0 +1,19 @@
+# Problem: 2161. Partition Array According to Given Pivot
+# LeetCode: https://leetcode.com/problems/partition-array-according-to-given-pivot/
+# Difficulty: Medium
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        n=len(nums)
+        if n==1: return nums
+        R=[]
+        l, m=0, 0
+        for x in nums:
+            if x<pivot:
+                nums[l]=x
+                l+=1
+            elif x>pivot:
+                R.append(x)
+            else:
+                m+=1
+        return nums[:l]+[pivot]*m+R
