dd9a487038fc212b49c3cfffa28a7e7d5f3fecc7
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-06-08 22:00:17 +0300

    feat(arrays): add solution for '164_Maximum_Gap.py'

---
+++ arrays/164_Maximum_Gap.py
@@ -0,0 +1,12 @@
+# Problem: 164. Maximum Gap
+# LeetCode: https://leetcode.com/problems/maximum-gap/
+# Difficulty: Medium
+
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        maxGap=0
+        nums.sort()
+        for i in range(1,len(nums)):
+            cur=abs(nums[i]-nums[i-1])
+            maxGap=max(maxGap,cur)
+        return 0 if len(nums)<2 else maxGap
