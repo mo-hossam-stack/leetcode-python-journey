26c1087129cd3ad1bb626e3d76398decf0160cd8
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-06-13 23:51:35 +0300

    feat(bit_manipulation): add solution for '3838_Weighted_Word_Mapping.py'

---
+++ bit_manipulation/3838_Weighted_Word_Mapping.py
@@ -0,0 +1,15 @@
+# Problem: 3838. Weighted Word Mapping
+# LeetCode: https://leetcode.com/problems/weighted-word-mapping/
+# Difficulty: Easy
+
+class Solution:
+    def mapWordWeights(self, words: List[str], wt: List[int]) -> str:
+        res = []
+
+        for word in words:
+            s = 0
+            for ch in word:
+                s += wt[(ord(ch) & (1 << 5) - 1) - 1]
+            res.append(chr(122 - (s - ((s * 2521) >> (1 << 4)) * len(wt))))
+
+        return "".join(res)
