4e9b9280363162bb9e029805c02fc5a1269f7f42
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-06-23 20:11:11 +0300

    feat(dp): add solution for '3699_Number_of_ZigZag_Arrays_I.py'

---
+++ dp/3699_Number_of_ZigZag_Arrays_I.py
@@ -0,0 +1,17 @@
+# Problem: 3699. Number of ZigZag Arrays I
+# LeetCode: https://leetcode.com/problems/number-of-zigzag-arrays-i/
+# Difficulty: Hard
+
+class Solution:
+    def zigZagArrays(self, n: int, l: int, r: int) -> int:
+        MOD = 1000000007
+        m = r - l + 1
+        dp = [1] * m
+
+        for i in range(2, n + 1):
+            dp.reverse()
+            s = 0
+            for j in range(m):
+                dp[j], s = s, (s + dp[j]) % MOD
+
+        return (sum(dp) % MOD << 1) % MOD
