c10ddaa8fa60f8723a882473e352a671098c96f2
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-06-25 12:04:35 +0300

    feat(arrays): add solution for '3737_Count_Subarrays_With_Majority_Element_I.py'

---
+++ arrays/3737_Count_Subarrays_With_Majority_Element_I.py
@@ -0,0 +1,22 @@
+# Problem: 3737. Count Subarrays With Majority Element I
+# LeetCode: https://leetcode.com/problems/count-subarrays-with-majority-element-i/
+# Difficulty: Medium
+
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        ans = 0
+
+        for l in range(n):
+            target_count = 0
+
+            for r in range(l, n):
+                if nums[r] == target:
+                    target_count += 1
+
+                length = r - l + 1
+
+                if target_count > length // 2:
+                    ans += 1
+
+        return ans
