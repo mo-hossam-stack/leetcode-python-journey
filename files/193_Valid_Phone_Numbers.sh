7324df4a4e7d09d55eea985e59c63b944632897f
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-07-07 23:11:21 +0300

    feat(files): add solution for '193_Valid_Phone_Numbers.sh'

---
+++ files/193_Valid_Phone_Numbers.sh
@@ -0,0 +1,5 @@
+# Problem: 193. Valid Phone Numbers
+# LeetCode: https://leetcode.com/problems/valid-phone-numbers/
+# Difficulty: Easy
+
+grep -E '^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$' file.txt
