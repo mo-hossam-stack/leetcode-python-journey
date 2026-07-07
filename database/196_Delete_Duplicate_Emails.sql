d32f5bf0e4f86605f4240754bb8320a5217cd8db
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-07-08 00:21:44 +0300

    feat(database): add solution for '196_Delete_Duplicate_Emails.sql'

---
+++ database/196_Delete_Duplicate_Emails.sql
@@ -0,0 +1,6 @@
+# Problem: 196. Delete Duplicate Emails
+# LeetCode: https://leetcode.com/problems/delete-duplicate-emails/
+# Difficulty: Easy
+
+delete p1 from person p1,person p2 
+where p1.email=p2.email and p1.id>p2.id;
