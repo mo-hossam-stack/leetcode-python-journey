b482b99c1bb437e96ec6da376bae0ae327a798e6
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-07-09 02:40:42 +0300

    feat(design): add solution for '227_Basic_Calculator_II.py'

---
+++ design/227_Basic_Calculator_II.py
@@ -0,0 +1,30 @@
+# Problem: 227. Basic Calculator II
+# LeetCode: https://leetcode.com/problems/basic-calculator-ii/
+# Difficulty: Medium
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        num = 0
+        prev_operator = '+'
+        
+        for i in range(len(s) + 1):
+            ch = s[i] if i < len(s) else '\0'
+            
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            
+            if not ch.isdigit() and ch != ' ' or i == len(s):
+                if prev_operator == '+':
+                    stack.append(num)
+                if prev_operator == '-':
+                    stack.append(-num)
+                if prev_operator == '*':
+                    stack.append(stack.pop() * num)
+                if prev_operator == '/':
+                    stack.append(int(stack.pop() / num))
+                
+                prev_operator = ch
+                num = 0
+        
+        return sum(stack)
