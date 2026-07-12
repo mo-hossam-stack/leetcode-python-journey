7ef21fe1ad8e01bde7375140d7168e805a3133b0
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-07-12 15:23:44 +0300

    feat(daily_challenges): july 11, 2026(miss)

---
+++ daily_challenges/2685_Count_the_Number_of_Complete_Components.py
@@ -0,0 +1,25 @@
+# Problem: 2685. Count the Number of Complete Components
+# LeetCode: https://leetcode.com/problems/count-the-number-of-complete-components/
+# Difficulty: Medium
+
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = [[] for _ in range(n)]
+        component_freq = defaultdict(int)
+
+        for vertex in range(n):
+            graph[vertex] = [vertex]
+
+        for v1, v2 in edges:
+            graph[v1].append(v2)
+            graph[v2].append(v1)
+
+        for vertex in range(n):
+            neighbors = tuple(sorted(graph[vertex]))
+            component_freq[neighbors] += 1
+
+        return sum(
+            1
+            for neighbors, freq in component_freq.items()
+            if len(neighbors) == freq
+        )
