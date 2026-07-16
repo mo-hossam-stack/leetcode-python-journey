6821a48e8d7939d7428cef3b1ba1be400363ae84
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-07-16 21:35:39 +0300

    feat(daily_challenges): July 16, 2026

---
+++ daily_challenges/3867_Sum_of_GCD_of_Formed_Pairs.py
@@ -0,0 +1,23 @@
+# Problem: 3867. Sum of GCD of Formed Pairs
+# LeetCode: https://leetcode.com/problems/sum-of-gcd-of-formed-pairs/
+# Difficulty: Medium
+
+class Solution:
+    def gcdSum(self, nums: list[int]) -> int:
+        mx = 0
+
+        for i in range(len(nums)):
+            mx = max(mx, nums[i])
+            nums[i] = gcd(mx, nums[i])
+
+        nums.sort()
+
+        left, right = 0, len(nums) - 1
+        ans = 0
+
+        while left < right:
+            ans += gcd(nums[left], nums[right])
+            left += 1
+            right -= 1
+
+        return ans
