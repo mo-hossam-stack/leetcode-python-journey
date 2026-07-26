ad1142c05f110dacda9a3f689a1c1c7e4134c47b
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-07-26 05:40:49 +0300

    feat(daily_challenges): July 26, 2026

---
+++ daily_challenges/628_Maximum_Product_of_Three_Numbers.py
@@ -0,0 +1,13 @@
+# Problem: 628. Maximum Product of Three Numbers
+# LeetCode: https://leetcode.com/problems/maximum-product-of-three-numbers/
+# Difficulty: Easy
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+
+        return max(
+            nums[n - 1] * nums[n - 2] * nums[n - 3],
+            nums[0] * nums[1] * nums[n - 1]
+        )
