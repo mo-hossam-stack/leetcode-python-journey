d74806ac438679e7cb1b9a00b441a3554520ef25
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-08-01 19:36:53 +0300

    feat(daily_challenges): August 01, 2026

---
+++ daily_challenges/486_Predict_the_Winner.py
@@ -0,0 +1,19 @@
+# Problem: 486. Predict the Winner
+# LeetCode: https://leetcode.com/problems/predict-the-winner/
+# Difficulty: Medium
+
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [0] * n
+
+        for left in range(n - 1, -1, -1):
+            dp[left] = nums[left]
+
+            for right in range(left + 1, n):
+                dp[right] = max(
+                    nums[left] - dp[right],
+                    nums[right] - dp[right - 1]
+                )
+
+        return dp[n - 1] >= 0
