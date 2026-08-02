43b0eefe8a8e3d20e4a24e1113413cba83184ee1
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-08-02 21:33:36 +0300

    feat(daily_challenges): August 02, 2026

---
+++ daily_challenges/877_Stone_Game.py
@@ -0,0 +1,7 @@
+# Problem: 877. Stone Game
+# LeetCode: https://leetcode.com/problems/stone-game/
+# Difficulty: Medium
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return 2>1
