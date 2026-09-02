84d70680ab3656c41643485173db76bf8b56a151
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-09-02 13:49:29 +0300

    feat(daily_challenges): September 02, 2026

---
+++ daily_challenges/3875_Construct_Uniform_Parity_Array_I.py
@@ -0,0 +1,6 @@
+# Problem: 3875. Construct Uniform Parity Array I
+# LeetCode: https://leetcode.com/problems/construct-uniform-parity-array-i/
+# Difficulty: Easy
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return 1==1
