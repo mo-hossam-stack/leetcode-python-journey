219d6fc038f22eea1fc2a135ad27b6e8837f8381
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-09-07 23:24:44 +0300

    feat(daily_challenges): September 07, 2026

---
+++ daily_challenges/940_Distinct_Subsequences_II.py
@@ -0,0 +1,20 @@
+# Problem: 940. Distinct Subsequences II
+# LeetCode: https://leetcode.com/problems/distinct-subsequences-ii/
+# Difficulty: Hard
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        MOD = 1000000007
+        
+        dp = 1
+        last = [0] * 26
+        
+        for ch in s:
+            index = ord(ch) - ord('a')
+            
+            old_dp = dp
+            
+            dp = (2 * dp - last[index] + MOD) % MOD
+            
+            last[index] = old_dp
+        
+        return (dp - 1 + MOD) % MOD
