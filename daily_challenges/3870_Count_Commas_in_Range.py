f9550789f2250be1bede8222b704bcb97f983879
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-09-08 20:28:30 +0300

    feat(daily_challenges): September 08, 2026

---
+++ daily_challenges/3870_Count_Commas_in_Range.py
@@ -0,0 +1,6 @@
+# Problem: 3870. Count Commas in Range
+# LeetCode: https://leetcode.com/problems/count-commas-in-range/
+# Difficulty: Easy
+class Solution:
+    def countCommas(self, n):
+        return max(0, n - 999)
