af8e28e25e9e7f1fc2651a4805be22ed2d8ffc99
Mohamed Hossam <hamo08521hamo@gmail.com>
2026-09-16 20:05:53 +0300

    feat(daily_challenges): September 16, 2026

---
+++ daily_challenges/1621_Number_of_Sets_of_K_Non_Overlapping_Line_Segments.py
@@ -0,0 +1,17 @@
+# Problem: 1621. Number of Sets of K Non-Overlapping Line Segments
+# LeetCode: https://leetcode.com/problems/number-of-sets-of-k-non-overlapping-line-segments/
+# Difficulty: Medium
+class Solution:
+    def numberOfSets(self, n: int, k: int) -> int:
+        MOD = 10**9 + 7
+        @lru_cache(None)
+        def dp(i, k, isStart):
+            if k == 0: return 1
+            if i == n: return 0
+            ans = dp(i+1, k, isStart) 
+            if isStart:
+                ans += dp(i+1, k, False) 
+            else:
+                ans += dp(i, k-1, True)
+            return ans % MOD
+        return dp(0, k, True)
